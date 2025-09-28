053ea54a59ec283db55c307c4531b22ebc13f104
Mohammed Abdur Rahman <mohammed.edu76@gmail.com>
2025-09-27 23:08:55 -0400

    Completed assignment 2

---
+++ Project2/Commands.sh
@@ -0,0 +1,10 @@
+java -cp . Assignment2 -h
+java -cp . Assignment2 -ln "3 4 5" -sm 4
+java -cp . Assignment2 -ln "3;4;5" -sm 2
+java -cp . Assignment2 -ln "1 1.0 2 2.0 3.0" -sm 3
+java -cp . Assignment2 -ln "-5 -1 0 1 5" -sm 4
+java -cp . Assignment2 -ln "2.5 2.5 3.5 3.5" -sm 3
+java -cp . Assignment2 -ln ".5 +3 0.5 1.5" -sm 3
+java -cp . Assignment2 -ln "trash 7 !!! 8.5 begin" -sm 4
+java -cp . Assignment2 -ln "" -sm 1
+java -cp . Assignment2 -ln "3 4 5" -sm 5
